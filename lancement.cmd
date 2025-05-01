fa138ee5208d575d94bf5d84b7671502a7eebb4a
ZPitor99 <gabriel.chifflet7845@outlook.fr>
2025-05-01 13:42:13 +0200

    execution app

--- lancement.cmd
+++ lancement.cmd
@@ -1,2 +1,8 @@
 @echo off
+echo =============================================
+echo Lanceur d'application Calendrier
+echo =============================================
+
+rem Tentative directe de lancement avec JavaFX SDK spécifique
+echo Tentative de lancement avec JavaFX SDK local...
 start javaw --module-path "C:\Program Files\openjfx-24_windows-x64_bin-sdk\javafx-sdk-24\lib" --add-modules javafx.controls,javafx.fxml -jar target/Calendrier-1.0.jar
